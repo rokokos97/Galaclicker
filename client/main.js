962ec9f0ebd41989ddd6e69f93347f205c258b97
Rostyslav Lisovyi <rokokos86@protonmail.com>
2025-02-12 18:42:46 +0100

    client:refactor: create constants.js and move LEVELS from main.js

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import { fetchUser, updateUser, fetchAllUsers } from './api/userApi';
+import { LEVELS } from './config/constants';
 
 
 
@@ -46,15 +47,7 @@ function initializeApp() {
 
     // const LEVELS = fetchLevels();
 
-    const LEVELS = [
-        { id: 1, name: "Student", numberOfCodeLines: 0, imgUrl: './galaStudent.webp', level: 1, maxLines: 100 },
-        { id: 2, name: "Trainee", numberOfCodeLines: 3000, imgUrl: './galaTrainee.webp', level: 1, maxLines: 200 },
-        { id: 3, name: "Junior", numberOfCodeLines: 100000, imgUrl: './galaJunior.webp', level: 5, maxLines: 200 },
-        { id: 4, name: "Middle", numberOfCodeLines: 1000000, imgUrl: './galaMiddle.webp', level: 10, maxLines: 400 },
-        { id: 5, name: "Senior", numberOfCodeLines: 10000000, imgUrl: './galaSenior.webp', level: 15, maxLines: 600 },
-        { id: 6, name: "Team Lead", numberOfCodeLines: 100000000, imgUrl: './galaTeamLead.webp', level: 25, maxLines: 800 },
-        { id: 7, name: "Google", numberOfCodeLines: 1000000000, imgUrl: './galaGoogle.webp', level: 50, maxLines: 999 }
-    ]
+
     function start (){
         checkAndResetDailyScore()
         checkAndResetMonthlyScore()
