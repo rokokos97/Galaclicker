0f7edaf512e199c132ad0e44a710f04d186e2591
Rostyslav Lisovyi <rokokos86@protonmail.com>
2025-02-18 18:31:03 +0100

    client:style: add user photo to user info

--- client/initUser.js
+++ client/initUser.js
@@ -3,12 +3,21 @@ import { storeUserData } from "./api/storeUserData/storeUserData";
 
 export function initUser(telegramUser) {
     const tg = window.Telegram?.WebApp;
+    const userPicUrl =  tg?.initDataUnsafe?.user?.photo_url
+    console.log('userPicUrl', userPicUrl);
+    if(userPicUrl){
+        const img = document.querySelector('.user__img-container img');
+        if (img) {
+            img.src = `${userPicUrl}`;
+        }
+    }
     console.log('tg', tg);
     if (!telegramUser) {
         console.warn('❗ Telegram WebApp API unavelo');
         startTestMode();
         return;
     }
+
     // console.log(`🔍 Initializing user with ID: ${telegramUser}...`);
     fetchUser(telegramUser)
         .then((dbUser) => {
